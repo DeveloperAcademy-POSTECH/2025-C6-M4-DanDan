034058581356ad6edb36bafaa606a12c9c455ea2
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-10-25 16:23:17 +0900

    chore: 초기 프로젝트 생성 및 폴더 구조 세팅

---
+++ DanDan/DanDan/Sources/Application/DanDanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanDanApp.swift
+//  DanDan
+//
+//  Created by Jay on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DanDanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
