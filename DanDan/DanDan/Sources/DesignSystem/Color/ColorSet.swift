96acc41aecd8b97cf96d560a719b2d4abf6baf09
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-10-31 01:06:48 +0900

    feat: 컬러셋 추가
    - enum으로 색상 관리

---
+++ DanDan/DanDan/Sources/DesignSystem/Color/ColorSet.swift
@@ -0,0 +1,15 @@
+//
+//  ColorSet.swift
+//  DanDan
+//
+//  Created by Jay on 10/31/25.
+//
+
+import SwiftUI
+
+enum ColorSet {
+    enum PointGreen: String {
+        case pointGreen01 = "PointGreen01"
+        case pointGreen02 = "PointGreen02"
+    }
+}
