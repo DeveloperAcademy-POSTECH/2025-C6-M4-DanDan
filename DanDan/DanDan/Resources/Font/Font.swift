30f477f5e2c54a53cafac3253ed9d30c030881fc
hiseyeon <se020122@naver.com>
2025-10-31 01:18:22 +0900

    fix: lineSpacing 제거 및 Dynamic Type 대응 (relativeTo) 적용 #36

--- DanDan/DanDan/Resources/Font/Font.swift
+++ DanDan/DanDan/Resources/Font/Font.swift
@@ -9,44 +9,23 @@ import SwiftUI
 
 /* 사용예시
     .font(.pretendard(.semiBold, size: 22))
- 
-     Text("철길숲 산책 기록")
-         .prText(Font.PR.title1)
+
+    .font(.PR.title1)
  */
 
-struct PRTextStyle {
-    let font: Font
-    let lineSpacing: CGFloat
-}
-
 extension Font {
-    static func pretendard(_ weight: PretendardWeight, size: CGFloat) -> Font {
-        .custom(weight.rawValue, size: size)
+    static func pretendard(_ weight: PretendardWeight, size: CGFloat, elativeTo textStyle: TextStyle = .body) -> Font {
+        .custom(weight.rawValue, size: size, relativeTo: textStyle)
     }
     
     enum PR {
-        static let title1 = PRTextStyle(font: .custom("Pretendard-Bold", size: 24), lineSpacing: 32)
-        static let title2 = PRTextStyle(font: .custom("Pretendard-SemiBold", size: 20), lineSpacing: 22)
-        static let body1  = PRTextStyle(font: .custom("Pretendard-SemiBold", size: 16), lineSpacing: 22)
-        static let body2  = PRTextStyle(font: .custom("Pretendard-Medium", size: 16), lineSpacing: 22)
-        static let caption1 = PRTextStyle(font: .custom("Pretendard-Regular", size: 15), lineSpacing: 22)
+        static let title1 = Font.custom("Pretendard-Bold", size: 24, relativeTo: .title)
+        static let title2 = Font.custom("Pretendard-SemiBold", size: 20, relativeTo: .title2)
+        static let body1 = Font.custom("Pretendard-SemiBold", size: 16, relativeTo: .body)
+        static let body2 = Font.custom("Pretendard-Medium", size: 16, relativeTo: .body)
+        static let caption1 = Font.custom("Pretendard-Regular", size: 15, relativeTo: .caption)
     }
-}
 
-struct PRTextModifier: ViewModifier {
-    let style: PRTextStyle
-    
-    func body(content: Content) -> some View {
-        content
-            .font(style.font)
-            .lineSpacing(style.lineSpacing)
-    }
-}
-
-extension View {
-    func prText(_ style: PRTextStyle) -> some View {
-        self.modifier(PRTextModifier(style: style))
-    }
 }
 
 enum PretendardWeight: String {
