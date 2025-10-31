e62b22d3a672f7cf443396c41a70943c2b1f93c8
hiseyeon <se020122@naver.com>
2025-10-31 15:34:46 +0900

    refactor: Point 모델의 coordinate, toGate() 메서드를 확장(extension)으로 분리 #20

--- DanDan/DanDan/Sources/Models/Point.swift
+++ DanDan/DanDan/Sources/Models/Point.swift
@@ -17,18 +17,4 @@ struct Point: Identifiable, Codable {
     var longitude: Double
     var bearingDeg: Double  // 구간 진행 방향 (도)
     var b_along: Double     // 게이트의 진행방향 반경 (미터)
-
-    /// CLLocationCoordinate2D로 변환
-    var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-    }
-
-    /// Point로부터 Gate 객체 생성
-    func toGate() -> Gate {
-        Gate(
-            center: coordinate,
-            bearingDeg: bearingDeg,
-            b_along: b_along
-        )
-    }
 }
