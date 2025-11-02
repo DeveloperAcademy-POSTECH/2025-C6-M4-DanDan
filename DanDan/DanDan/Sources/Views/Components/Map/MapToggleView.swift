0635736c2b606f4d7a69f46fa2f3e1b443141388
Yeonsoo So <janu10.sys@gmail.com>
2025-11-02 11:21:34 +0900

    feat: MapToggleButton 구현 #52

---
+++ DanDan/DanDan/Sources/Views/Components/Map/MapToggleView.swift
@@ -0,0 +1,60 @@
+//
+//  MapToggleView.swift
+//  DanDan
+//
+//  Created by soyeonsoo on 11/2/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapToggleView: View {
+    @State private var isFullMap = false
+    
+    var body: some View {
+        ZStack {
+            Group {
+                if isFullMap {
+                    FullMapView() // 2D 전체 지도뷰
+                } else {
+                    MapView() // 3D 부분 지도뷰
+                }
+            }
+            .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Button {
+                        withAnimation(.snappy(duration: 0.25)) {
+                            isFullMap.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "globe.central.south.asia.fill")
+                            .font(.system(size: 22, weight: .semibold))
+                        
+                            // TODO: 컬러셋 추가 후 활성화 시 아이콘 색상 변경
+                        
+                            .foregroundStyle(isFullMap ? .red : .black)
+                            .frame(width: 56, height: 56)
+                    }
+                    .buttonStyle(.plain)
+                    .background(.ultraThinMaterial, in: Circle())
+                    .overlay(
+                        Circle()
+                            .strokeBorder(.white.opacity(0.4), lineWidth: 1)
+                    )
+                    .shadow(color: .black.opacity(0.15), radius: 12, x: 0, y: 6)
+                    .padding(.trailing, 20)
+                }
+                .padding(.top, 100)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview{
+    MapToggleView()
+}
