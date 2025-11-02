395a49fa91e72409d63b9ba446a73747d4d976cd
Yeonsoo So <janu10.sys@gmail.com>
2025-11-02 13:59:42 +0900

    refactor: UI 컴포넌트 뷰에서 로직 분리  - RankingViewModel로 #45

--- DanDan/DanDan/Sources/Views/Components/Ranking/RankingItemView.swift
+++ DanDan/DanDan/Sources/Views/Components/Ranking/RankingItemView.swift
@@ -13,31 +13,32 @@ struct RankingItemView: View {
     var userImage: UIImage?
     var userConqueredZone: Int
     var userTeam: String
+    var backgroundColor: Color
     
-    // MARK: - Initializer (도메인 모델 -> 뷰 데이터로 매핑)
-    /// 실제 모델(UserStatus, UserInfo)을 받아서 UI에 필요한 값만 분리해 저장
-    init(status: UserStatus, info: UserInfo) {
-        self.ranking = status.rank
-        self.userName = info.userName
-        
-        if let firstData = info.userImage.first,
-           let uiImage = UIImage(data: firstData) {
-            self.userImage = uiImage
-        } else {
-            self.userImage = nil
-        }
-        
-        self.userConqueredZone = status.userDailyScore
-        self.userTeam = status.userTeam
+    init(data: RankingItemData) {
+        self.ranking = data.ranking
+        self.userName = data.userName
+        self.userImage = data.userImage
+        self.userConqueredZone = data.userConqueredZone
+        self.userTeam = data.userTeam
+        self.backgroundColor = data.backgroundColor
     }
-
+    
     /// Preview를 위한 편의 init
-    init(ranking: Int, userName: String, userImage: UIImage? = nil, userConqueredZone: Int, userTeam: String = "none") {
+    init(
+        ranking: Int,
+        userName: String,
+        userImage: UIImage? = nil,
+        userConqueredZone: Int,
+        userTeam: String = "none",
+        backgroundColor: Color
+    ) {
         self.ranking = ranking
         self.userName = userName
         self.userImage = userImage
         self.userConqueredZone = userConqueredZone
         self.userTeam = userTeam
+        self.backgroundColor = backgroundColor
     }
     
     // TODO: 폰트셋 추가 후 수정
@@ -63,31 +64,16 @@ struct RankingItemView: View {
                 .foregroundStyle(.gray)
         }
         .frame(maxWidth: 353, maxHeight: 78)
-        // TODO: 컬러셋 추가 후 수정
         .background(backgroundColor)
         .cornerRadius(12)
     }
-    
-    // TODO: 팀명 및 컬러셋 확정 후 수정
-    // MARK: - Background Logic by Team
-    /// 팀에 따라 랭킹 아이템 배경색을 다르게 지정
-    private var backgroundColor: Color {
-        switch userTeam.lowercased() {
-        case "blue":
-            return Color.blue.opacity(0.1)
-        case "yellow":
-            return Color.yellow.opacity(0.1)
-        default:
-            return Color.gray.opacity(0.1)
-        }
-    }
 }
 
-//#Preview {
-//    VStack(spacing: 8) {
-//        RankingItemView(ranking: 8, userName: "소연수", userConqueredZone: 12, userTeam: "blue")
-//        RankingItemView(ranking: 9, userName: "김소원", userConqueredZone: 9, userTeam: "blue")
-//        RankingItemView(ranking: 10, userName: "허찬욱", userConqueredZone: 7, userTeam: "yellow")
-//    }
-//    .padding()
-//}
+#Preview {
+    VStack(spacing: 8) {
+        RankingItemView(ranking: 8, userName: "소연수", userConqueredZone: 12, userTeam: "blue", backgroundColor: .blue.opacity(0.1))
+        RankingItemView(ranking: 9, userName: "김소원", userConqueredZone: 9, userTeam: "blue", backgroundColor: .blue.opacity(0.1))
+        RankingItemView(ranking: 10, userName: "허찬욱", userConqueredZone: 7, userTeam: "yellow", backgroundColor: .yellow.opacity(0.1))
+    }
+    .padding()
+}
