b0ccb90bada9c4ef31d4f05bac5f84e8b5b9659b
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-11-02 20:09:49 +0900

    feat: 팀 랭킹 UI 분리
    - Subviews 폴더에 TeamRankView 추가

--- DanDan/DanDan/Sources/Views/RankingView/RankingView.swift
+++ DanDan/DanDan/Sources/Views/RankingView/RankingView.swift
@@ -8,34 +8,16 @@
 import SwiftUI
 
 struct RankingView: View {
-    @StateObject var viewModel = RankingViewModel()
+    @StateObject private var viewModel = RankingViewModel()
 
     var body: some View {
         VStack {
-            List(viewModel.conquestStatuses) { status in
-                Text("Zone \(status.zoneId): \(status.teamName ?? "무승부")")
-            }
-
-            Button {
-                viewModel.tapMainButton()
-            } label: {
-                Text("홈")
-            }
+            TeamRankView()
         }
         .padding()
     }
 }
 
 #Preview {
-    let previewViewModel = RankingViewModel()
-    let dummyScores: [ZoneScore] = [
-        ZoneScore(zoneId: 1, teamId: 1, teamName: "Team A", teamScore: 5),
-        ZoneScore(zoneId: 1, teamId: 2, teamName: "Team B", teamScore: 3),
-        ZoneScore(zoneId: 2, teamId: 1, teamName: "Team A", teamScore: 2),
-        ZoneScore(zoneId: 2, teamId: 2, teamName: "Team B", teamScore: 2),
-        ZoneScore(zoneId: 3, teamId: 2, teamName: "Team B", teamScore: 7),
-    ]
-    previewViewModel.updateConquestStatuses(with: dummyScores)
-
-    return RankingView(viewModel: previewViewModel)
+    RankingView()
 }
---
+++ DanDan/DanDan/Sources/Views/RankingView/SubViews/TeamRankView.swift
@@ -0,0 +1,14 @@
+//
+//  TeamRankView.swift
+//  DanDan
+//
+//  Created by Jay on 11/2/25.
+//
+
+import SwiftUI
+
+struct TeamRankView: View {
+    var body: some View {
+        Text("TeamRankView")
+    }
+}
