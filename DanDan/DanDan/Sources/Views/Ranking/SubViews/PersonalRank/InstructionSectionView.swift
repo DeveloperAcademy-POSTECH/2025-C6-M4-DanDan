6e352c0488a2c474eb205a96be574d986a0e5758
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-11-03 12:10:54 +0900

    feat: Dropdown 컴포넌트 구현

--- DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/InstructionSectionView.swift
+++ DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/InstructionSectionView.swift
@@ -8,56 +8,19 @@
 import SwiftUI
 
 struct InstructionSectionView: View {
-
-    @State private var isDropdownExpanded: Bool = false
-    @State private var selectedOption: String = "전체"
-
-    let title: String = "개인랭킹"
-    let options: [String] = ["전체", "우리팀", "상위 10명"]
-
+    @State private var selected = "전체"
+    
     var body: some View {
         HStack {
             Text("개인랭킹")
+                .font(.PR.title1)
 
             Spacer()
 
-            Button {
-                withAnimation {
-                    isDropdownExpanded.toggle()
-                }
-            } label: {
-                HStack(spacing: 4) {
-                    Text(selectedOption)
-                        .font(.system(size: 14))
-                        .foregroundColor(Color("PointGreen01"))  // 너희 색상 시스템 사용 시
-                    Image(systemName: "chevron.down")
-                        .resizable()
-                        .frame(width: 10, height: 5)
-                        .foregroundColor(Color("pointGreen01"))
-                        .rotationEffect(.degrees(isDropdownExpanded ? 180 : 0))
-                }
-            }
-        }
-        
-        if isDropdownExpanded {
-            VStack(alignment: .leading, spacing: 6) {
-                ForEach(options, id: \.self) { option in
-                    Button {
-                        selectedOption = option
-                        withAnimation {
-                            isDropdownExpanded = false
-                        }
-                    } label: {
-                        Text(option)
-                            .padding(.vertical, 6)
-                            .padding(.horizontal, 12)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .background(Color.gray.opacity(0.05))
-                            .cornerRadius(6)
-                    }
-                }
-            }
-            .padding(.horizontal, 20)
+            PickerMenu(
+                selectedOption: $selected,
+                options: ["전체", "우리 팀"]
+            )
         }
     }
 }
