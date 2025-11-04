4be795a52728ab154259a661bddbbb9b21503da0
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-11-04 21:01:07 +0900

    feat: 컴포넌트 구현

---
+++ DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/RankingList/Component/RankingChangeIndicatorView.swift
@@ -0,0 +1,29 @@
+//
+//  RankingChangeIndicatorView.swift
+//  DanDan
+//
+//  Created by Jay on 11/4/25.
+//
+
+import SwiftUI
+
+struct RankingChangeIndicatorView: View {
+    let rankDiff: Int
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            Image(rankDiff > 0 ? "rank_up" : "rank_down")
+                .resizable()
+                .frame(width: 8, height: 8)
+            
+            Text("\(abs(rankDiff))")
+                .font(.PR.body1)
+                .foregroundStyle(.gray1)
+        }
+    }
+}
+
+#Preview {
+    RankingChangeIndicatorView(rankDiff: 4)
+    RankingChangeIndicatorView(rankDiff: -4)
+}
