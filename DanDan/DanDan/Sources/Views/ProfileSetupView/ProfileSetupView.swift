6bbe3daae14e8cac92fd43e198d8831c85ff969f
Yeonsoo So <janu10.sys@gmail.com>
2025-11-06 00:13:21 +0900

    feat: 컴포넌트 적용 및 네비게이션 연결 #108

--- DanDan/DanDan/Sources/Views/ProfileSetupView/ProfileSetupView.swift
+++ DanDan/DanDan/Sources/Views/ProfileSetupView/ProfileSetupView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import PhotosUI
 
 struct ProfileSetupView: View {
+    @EnvironmentObject private var nav: NavigationManager
+    
     @State private var nickname: String = "" // 서버에 전송할 닉네임
     @State private var selectedItem: PhotosPickerItem?
     @State private var profileImage: UIImage? // 서버에 전송할 이미지
@@ -20,54 +22,44 @@ struct ProfileSetupView: View {
     var onSave: ((_ nickname: String, _ image: UIImage?) -> Void)?
     
     var body: some View {
-        ZStack {
-            Color(.systemBackground).ignoresSafeArea()
+        
+        VStack(alignment: .leading, spacing: 24) {
             
-            VStack(alignment: .leading, spacing: 24) {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("회원가입하기")
-                        .font(.PR.title1)
-                        .foregroundStyle(.steelBlack)
-                    
-                    Text("나만의 닉네임과 프로필을 설정해주세요.")
-                        .font(.PR.caption3)
-                        .foregroundStyle(.gray2)
-                        .lineSpacing(3)
-                }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 10)
+            TitleSectionView(title: "회원가입하기", description: "나만의 닉네임과 프로필을 설정해주세요.")
                 .padding(.top, 58)
-                
-                VStack(spacing: 20) {
-                    // 프로필 이미지 업로드 버튼
-                    AvatarEditButton(
-                        image: profileImage,
-                        diameter: 120,
-                        overlayHeight: 38,
-                        overlayColor: .darkGreen.opacity(0.8)
-                    ) {
-                        showPicker = true
-                    }
-                    .photosPicker(isPresented: $showPicker, selection: $selectedItem, matching: .images)
-                    .padding(.vertical, 10)
-                    
-                    CustomTextField(text: $nickname, prompt: "닉네임 입력")
+            
+            VStack(spacing: 20) {
+                // 프로필 이미지 업로드 버튼
+                AvatarEditButton(
+                    image: profileImage,
+                    diameter: 120,
+                    overlayHeight: 38,
+                    overlayColor: .darkGreen.opacity(0.8)
+                ) {
+                    showPicker = true
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.top, 8)
+                .photosPicker(isPresented: $showPicker, selection: $selectedItem, matching: .images)
+                .padding(.vertical, 10)
                 
-                Spacer()
-                
-                PrimaryButton(
-                    "저장하기",
-                    action: { onSave?(nickname, profileImage) }, // 여기서 서버 API 호출
-                    horizontalPadding: 20,
-                    verticalPadding: 8,
-                )
-                .padding(.bottom, 24)
+                CustomTextField(text: $nickname, prompt: "닉네임 입력")
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .frame(maxWidth: .infinity)
+            .padding(.top, 8)
+            
+            Spacer()
+            
+            PrimaryButton(
+                "저장하기",
+                action: {
+                    onSave?(nickname, profileImage)
+                    nav.navigate(to: .schoolSelection)
+                }, // 여기서 서버 API 호출
+                horizontalPadding: 20,
+                verticalPadding: 8,
+            )
+            .padding(.bottom, 24)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .onChange(of: selectedItem) { _, newValue in
             guard let item = newValue else { return }
             Task {
