74dcd5ce7be099f15cdc764e813fd9eb83a99fd7
SingleBungIe <50876959+SingleBungIe@users.noreply.github.com>
2025-11-05 19:10:16 +0900

    feat: 백과 로직 중복으로 조치를 위한 중단

--- DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/RankingCardSection/RankingCardSectionView.swift
+++ DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/RankingCardSection/RankingCardSectionView.swift
@@ -8,33 +8,72 @@
 import SwiftUI
 
 struct RankingCardSectionView: View {
+    let rankingItems: [RankingViewModel.RankingItemData]
+    let myUserId: UUID
+
     var body: some View {
         HStack(spacing: 12) {
-            // TODO: 랭킹카드 로직 연결 필요
-            RankingCard(
-                name: "황세연",
-                score: 9,
-                image: Image("testImage"),
-                color: .subB
+            let topThree = Array(
+                rankingItems.sorted { $0.ranking < $1.ranking }.prefix(3)
             )
 
             RankingCard(
-                name: "해피",
-                score: 1
+                name: topThree[1].userName,
+                score: topThree[1].userWeekScore,
+                image: Image(uiImage: topThree[1].userImage ?? UIImage()),
+                color: topThree[1].backgroundColor
+            )
+
+            RankingCard(
+                name: topThree[0].userName,
+                score: topThree[0].userWeekScore,
+                image: Image(uiImage: topThree[0].userImage ?? UIImage()),
+                color: topThree[0].backgroundColor
             )
 
             .padding(.bottom, 20)
 
             RankingCard(
-                name: "황세연",
-                score: 9,
-                image: Image("testImage"),
-                color: .subA
+                name: topThree[2].userName,
+                score: topThree[2].userWeekScore,
+                image: Image(uiImage: topThree[2].userImage ?? UIImage()),
+                color: topThree[2].backgroundColor
             )
         }
     }
 }
 
 #Preview {
-    RankingCardSectionView()
+    RankingCardSectionView(
+        rankingItems: [
+            .init(
+                id: UUID(),
+                ranking: 1,
+                userName: "소연수",
+                userImage: nil,
+                userWeekScore: 12,
+                userTeam: "blue",
+                backgroundColor: .subA20
+            ),
+            .init(
+                id: UUID(),
+                ranking: 2,
+                userName: "김소원",
+                userImage: nil,
+                userWeekScore: 9,
+                userTeam: "blue",
+                backgroundColor: .subA20
+            ),
+            .init(
+                id: UUID(),
+                ranking: 3,
+                userName: "허찬욱",
+                userImage: nil,
+                userWeekScore: 7,
+                userTeam: "yellow",
+                backgroundColor: .subB20
+            ),
+        ],
+        myUserId: UUID()
+    )
 }
--- DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/RankingList/RankingListView.swift
+++ DanDan/DanDan/Sources/Views/Ranking/SubViews/PersonalRank/RankingList/RankingListView.swift
@@ -5,19 +5,27 @@
 //  Created by Jay on 11/3/25.
 //
 
-
 import SwiftUI
 
 struct RankingListView: View {
     let rankingItems: [RankingViewModel.RankingItemData]
     let myUserId: UUID
 
+    private var topThreeItems: [RankingViewModel.RankingItemData] {
+        Array(rankingItems.sorted { $0.ranking < $1.ranking }.prefix(3))
+    }
+
     var body: some View {
         ScrollView {
             VStack(spacing: 8) {
-                RankingCardSectionView()
+                if !topThreeItems.isEmpty {
+                    RankingCardSectionView(
+                        rankingItems: topThreeItems,
+                        myUserId: myUserId
+                    )
                     .padding(.vertical, 36)
-                
+                }
+
                 ForEach(rankingItems) { item in
                     RankingItemView(
                         rank: item,
@@ -32,9 +40,33 @@ struct RankingListView: View {
 #Preview {
     RankingListView(
         rankingItems: [
-            .init(id: UUID(uuidString: "11111111-1111-1111-1111-111111111111")!, ranking: 1, userName: "소연수", userImage: nil, userWeekScore: 12, userTeam: "blue", backgroundColor: .blue.opacity(0.1)),
-            .init(id: UUID(uuidString: "22222222-2222-2222-2222-222222222222")!, ranking: 2, userName: "김소원", userImage: nil, userWeekScore: 9, userTeam: "blue", backgroundColor: .blue.opacity(0.1)),
-            .init(id: UUID(uuidString: "33333333-3333-3333-3333-333333333333")!, ranking: 3, userName: "허찬욱", userImage: nil, userWeekScore: 7, userTeam: "yellow", backgroundColor: .yellow.opacity(0.1))
+            .init(
+                id: UUID(uuidString: "11111111-1111-1111-1111-111111111111")!,
+                ranking: 1,
+                userName: "소연수",
+                userImage: nil,
+                userWeekScore: 12,
+                userTeam: "blue",
+                backgroundColor: .blue.opacity(0.1)
+            ),
+            .init(
+                id: UUID(uuidString: "22222222-2222-2222-2222-222222222222")!,
+                ranking: 2,
+                userName: "김소원",
+                userImage: nil,
+                userWeekScore: 9,
+                userTeam: "blue",
+                backgroundColor: .blue.opacity(0.1)
+            ),
+            .init(
+                id: UUID(uuidString: "33333333-3333-3333-3333-333333333333")!,
+                ranking: 3,
+                userName: "허찬욱",
+                userImage: nil,
+                userWeekScore: 7,
+                userTeam: "yellow",
+                backgroundColor: .yellow.opacity(0.1)
+            ),
         ],
         myUserId: UUID(uuidString: "22222222-2222-2222-2222-222222222222")!
     )
