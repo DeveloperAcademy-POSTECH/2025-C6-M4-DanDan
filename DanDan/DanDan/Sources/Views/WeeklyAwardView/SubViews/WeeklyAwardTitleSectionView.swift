7fa5ba7e92883f4034a862fdba6baf20da13ba98
Yeonsoo So <janu10.sys@gmail.com>
2025-11-07 11:24:07 +0900

    feat: 서브뷰 구현 #118

---
+++ DanDan/DanDan/Sources/Views/WeeklyAwardView/SubViews/WeeklyAwardTitleSectionView.swift
@@ -0,0 +1,27 @@
+//
+//  WeeklyAwardTitleSectionView.swift
+//  DanDan
+//
+//  Created by soyeonsoo on 11/7/25.
+//
+
+import SwiftUI
+
+struct WeeklyAwardTitleSectionView: View {
+    var title: String
+    var description: String
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Text(title)
+                .font(.PR.title1)
+                .foregroundStyle(.steelBlack)
+            
+            Text(description)
+                .font(.PR.caption3)
+                .foregroundStyle(.gray2)
+                .lineSpacing(3)
+        }
+        .padding(.top, 68)
+    }
+}
