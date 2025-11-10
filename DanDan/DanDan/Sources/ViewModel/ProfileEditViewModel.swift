1858596409422685a570044aabd334c857dbc78b
hiseyeon <se020122@naver.com>
2025-11-10 11:55:19 +0900

    feat: ProfileEditViewModel 추가 #59

---
+++ DanDan/DanDan/Sources/ViewModel/ProfileEditViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditViewModel.swift
+//  DanDan
+//
+//  Created by Hwnag Seyeon on 11/10/25.
+//
+
+import SwiftUI
+
+struct ProfileEditViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditViewModel()
+}
