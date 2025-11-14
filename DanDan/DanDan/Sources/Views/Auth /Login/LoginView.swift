6a4a705412b20af31c9b08bacf34e6d846adc5bb
WISH <wishful.to@gmail.com>
2025-11-14 21:45:46 +0900

    refactor: LottieLoop 분리
    반복되는 로띠 사용시마다 이용하기 위해 로그인뷰에서의 LottieLoopView 함수를 LottieLoop로 분리

--- DanDan/DanDan/Sources/Views/Auth /Login/LoginView.swift
+++ DanDan/DanDan/Sources/Views/Auth /Login/LoginView.swift
@@ -31,26 +31,6 @@ struct LoginView: View {
             Text("테스트 버전이라 게스트로 로그인만 가능해요")
         }
     }
-    
-    private struct LottieLoopView: UIViewRepresentable {
-            let name: String
-            var contentMode: UIView.ContentMode = .scaleAspectFill
-            
-            func makeUIView(context: Context) -> LottieAnimationView {
-                let animationView = LottieAnimationView(name: name)
-                animationView.loopMode = .loop
-                animationView.contentMode = contentMode
-                animationView.backgroundBehavior = .pauseAndRestore
-                animationView.isUserInteractionEnabled = false
-                animationView.clipsToBounds = true
-                animationView.layer.allowsEdgeAntialiasing = true
-                animationView.play()
-                return animationView
-            }
-
-            func updateUIView(_ uiView: LottieAnimationView, context: Context) {}
-        }
-    
 }
 
 // MARK: - Preview
