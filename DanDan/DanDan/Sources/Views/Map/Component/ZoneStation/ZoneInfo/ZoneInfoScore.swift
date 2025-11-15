54c9bf18d7bb51dee5d238cd19c4ca6c3f6901ca
hiseyeon <se020122@naver.com>
2025-11-15 20:07:34 +0900

    feat: zoneInfoView 컴포넌트 생성
    - zoneInfoDescription
    - zoneInfoScore
    - zoneInfoTitle

---
+++ DanDan/DanDan/Sources/Views/Map/Component/ZoneStation/ZoneInfo/ZoneInfoScore.swift
@@ -0,0 +1,40 @@
+//
+//  ZoneInfoScore.swift
+//  DanDan
+//
+//  Created by Hwnag Seyeon on 11/15/25.
+//
+
+import SwiftUI
+
+struct ZoneInfoScore: View {
+    let teamScores: [ZoneTeamScoreDTO]
+    let maxTeamsToShow: Int?
+    
+    var body: some View {
+        VStack(spacing: 8){
+            HStack(spacing: 8){
+                
+                ForEach(teamScores.indices, id: \.self) { idx in
+                    Text(teamScores[idx].teamName)
+                        .font(.PR.title2)
+                        .foregroundColor(.gray1)
+                }
+            }
+            
+            HStack(spacing: 0){
+                if teamScores.isEmpty {
+                    Text("— : —")
+                        .font(.PR.title1)
+                        .foregroundColor(.darkGreen)
+                } else {
+                    Text(teamScores
+                        .map { String($0.totalScore) }
+                        .joined(separator: " : "))
+                        .font(.PR.title1)
+                        .foregroundColor(.darkGreen)
+                }
+            }
+        }
+    }
+}
