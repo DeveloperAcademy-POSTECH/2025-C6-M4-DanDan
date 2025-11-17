8dde973017f32fe8ed720717cc43cffffac94971
Yeonsoo So <janu10.sys@gmail.com>
2025-11-17 23:10:24 +0900

    chore: LottieOnceView에 onCompleted 파라미터 추가 #186

--- DanDan/DanDan/Sources/DesignSystem/Lottie/LottieOnce.swift
+++ DanDan/DanDan/Sources/DesignSystem/Lottie/LottieOnce.swift
@@ -12,7 +12,8 @@ struct LottieOnceView: UIViewRepresentable {
     let name: String
     var contentMode: UIView.ContentMode = .scaleAspectFit
     var holdProgress: CGFloat = 0.99
-
+    var onCompleted: (() -> Void)? = nil
+    
     func makeUIView(context: Context) -> LottieAnimationView {
         let view = LottieAnimationView(name: name)
         view.loopMode = .playOnce
@@ -21,13 +22,20 @@ struct LottieOnceView: UIViewRepresentable {
         view.isUserInteractionEnabled = false
         view.clipsToBounds = true
         view.layer.allowsEdgeAntialiasing = true
-
+        
         // 0 -> 0.99까지만 재생하고 해당 지점에서 정지 (마지막 프레임이 투명이라서)
-        view.play(fromProgress: 0, toProgress: holdProgress, loopMode: .playOnce) { _ in
-            view.pause()
+        view.play(fromProgress: 0, toProgress: holdProgress, loopMode: .playOnce) { finished in
+            if finished {
+                view.currentProgress = holdProgress
+                view.pause()
+                // SwiftUI 상태 업데이트는 main에서
+                DispatchQueue.main.async {
+                    onCompleted?()
+                }
+            }
         }
         return view
     }
-
+    
     func updateUIView(_ uiView: LottieAnimationView, context: Context) {}
 }
