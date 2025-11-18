781db668aaeeebc8a650de3f1d833fd54b744364
WISH <wishful.to@gmail.com>
2025-11-18 16:59:13 +0900

    design: 디자인 시안 규격 반영

--- DanDan/DanDan/Sources/Views/Map/Component/ZoneStation/ZoneInfo/ZoneInfoDescription.swift
+++ DanDan/DanDan/Sources/Views/Map/Component/ZoneStation/ZoneInfo/ZoneInfoDescription.swift
@@ -8,20 +8,22 @@
 import SwiftUI
 
 struct ZoneInfoDescription: View {
-    let distance: Int 
+    let distance: Int
     let descriptionText: String
-
+    
     var body: some View {
         VStack(spacing: 0) {
-
-                    Text("\(distance)m")
-                        .font(.PR.caption3)
-                        .foregroundColor(.gray1)
-
-                    Text(descriptionText)
-                        .font(.PR.caption3)
-                        .foregroundColor(.gray1)
-                }
-                .frame(maxWidth: .infinity)
+            Text("\(distance)m")
+                .font(.PR.caption3)
+                .foregroundColor(.gray1)
+                .frame(height: 22)
+            
+            Text(descriptionText)
+                .font(.PR.caption3)
+                .foregroundColor(.gray1)
+                .frame(height: 22)
+            
+        }
+        .frame(maxWidth: .infinity)
     }
 }
